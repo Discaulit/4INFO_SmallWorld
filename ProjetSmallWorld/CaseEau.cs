9f1586bb8c2bdb19195c8850fbb26a96b2fbf2b3
unknown <fdubar@PC2-INFO8.educ.insa>
2013-09-26 13:23:25 +0200

    Commit initial avec tous les fichiers

---
+++ ProjetSmallWorld/CaseEau.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetSmallWorld
+{
+    public interface CaseEau : TypeCase
+    {
+    }
+
+    public interface CopyOfCaseEau
+    {
+    }
+}
